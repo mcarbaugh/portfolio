383a88054ff6f91230e291ee32d086f52c134f9f
mcarbaugh <michael.s.carbaugh@gmail.com>
2018-05-21 23:59:55 -0400

    Added support for redux dev tools

--- src/state/store.tsx
+++ src/state/store.tsx
@@ -1,9 +1,21 @@
-import { createStore } from 'redux';
+import { applyMiddleware, createStore, compose } from 'redux';
 import { rootReducer } from './reducer';
+
+// tslint:disable
+declare global {
+    interface Window { __REDUX_DEVTOOLS_EXTENSION_COMPOSE__: any; }
+}
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = () => {
     return createStore(
         rootReducer,
+        composeEnhancers(
+            applyMiddleware(
+                // ...middleware,
+            ),
+        ),
     );
 };
 
